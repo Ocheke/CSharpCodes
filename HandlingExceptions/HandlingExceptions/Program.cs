5b3b56ca1195a1fab07484054763c6a95387a52a
Ocheke <ochekeglady@gmail.com>
2022-07-08 13:54:12 +0100

    A new project

---
+++ HandlingExceptions/HandlingExceptions/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+try
+{
+    Console.WriteLine("enter first number");
+    string num1 = Console.ReadLine();
+    Console.WriteLine("enter second number");
+    string num2 = Console.ReadLine();
+    Console.WriteLine((decimal)int.Parse(num1) / int.Parse(num2));
+}
+catch(FormatException)
+{
+    Console.WriteLine("Only numbers allowed");
+}
+catch(DivideByZeroException)
+{
+    Console.WriteLine("You can't divide by zero");
+}
+catch(Exception err)
+{
+    Console.WriteLine(err);
+}
+finally
+{
+    Console.WriteLine("Thanks for trying......");
+}
